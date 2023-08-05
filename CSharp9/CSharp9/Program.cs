40c78b2c408336b688517657d39fddb923292a4f
Reza <rbasereh@gmail.com>
2023-08-05 19:23:42 +0330

    Collections

---
+++ CSharp9/CSharp9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Globalization;
+
+namespace CSharp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int q = 0;
+            int w = 0;
+            int e = 0;
+            Calc1(q);
+            Calc2(ref w);
+            Calc3(out e);
+        }
+        public static void Calc1(int x)
+        {
+            x++;
+        }
+        public static void Calc2(ref int x)
+        {
+            x++;
+        }
+        public static void Calc3(out int x)
+        {
+            x = 0;
+            x++;
+        }
+    }
+
+}
---
+++ CSharp9/CSharp9/RefOut.cs
@@ -0,0 +1,125 @@
+﻿//using System.Collections;
+//using System.Globalization;
+
+//namespace CSharp9
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+
+//            ArrayList arrayList = new();
+
+//            //IEnumerable<User> users1 = users;
+//            //ICollection<User> users2 = users;
+//            //IList<User> users3 = users;
+
+//            //users2.Add(new User());
+
+//            ////var result2 = users2[1];
+//            ////var result = users3[1];
+
+//            //Queue queue = new();
+//            //Queue<User> UserQueue;
+
+//            //Stack stack = new();
+//            //Stack<User> UserStack;
+
+//            //queue.Enqueue(new Program());
+//            //queue.Enqueue(new User());
+//            List<User> users = new();
+
+//            User UserAli = new User() { Name = "Ali", NCode = "888" };
+//            User UserHasan = new User() { Name = "Hasan", NCode = "999" };
+//            User UserReza = new User() { Name = "Reza", NCode = "777" };
+
+
+//            users.Add(UserHasan);
+//            users.Add(UserAli);
+//            users.Add(UserReza);
+
+//            users.Remove(UserAli);
+//            users.RemoveAt(1);
+//            //var User0 = users[0];
+//            //var User1 = users[1];
+//            //var User2 = users[2];
+
+
+
+//            Dictionary<string, User> UserDictionary = new();
+
+//            UserDictionary.Add("999", UserHasan);
+//            UserDictionary.Add("777", UserAli);
+//            UserDictionary.Add("111", UserAli);
+//            UserDictionary.Add("110", UserAli);
+//            UserDictionary.Add("112", UserAli);
+//            UserDictionary.Add("113", UserAli);
+//            //UserDictionary.Add("888",UserReza);
+//            var resultAdd = UserDictionary.TryAdd("888", UserReza);
+//            var resultAdd2 = UserDictionary.TryAdd("888", UserReza);
+
+
+//            var User0 = UserDictionary["999"];
+//            var User1 = UserDictionary["777"];
+//            var User2 = UserDictionary["888"];
+
+//            //var User3 = UserDictionary["987"];
+
+//            var result = UserDictionary.TryGetValue("888", out User user);
+//            Console.Write(user.LName);
+
+//            var result2 = UserDictionary.TryGetValue("987", out User user2);
+//            if (user2 != null)
+//                Console.Write(user2.LName);
+
+//            Console.Write(user2.LName);
+
+//            UserDictionary.Remove("999");
+
+
+//            User findAli;
+//            foreach (var item in UserDictionary)
+//            {
+//                if (item.Value.Name == "Ali")
+//                {
+//                    findAli = item.Value;
+//                }
+//            }
+//            //var FindAli2 = UserDictionary.First(e => e.Value.Name == "Ali");
+
+//            Dictionary<bool, User> dict = new();
+//            dict.Add(true, UserAli);
+//            dict.TryAdd(true, UserHasan);
+//            dict.Add(false, UserAli);
+
+//            Dictionary<UserStatus, User> dict2 = new();
+//            dict2.Add(UserStatus.Active, UserAli);
+//            dict2.TryAdd(UserStatus.Draft, UserHasan);
+//            dict2.Add(UserStatus.Inactive, UserAli);
+
+
+//        }
+//    }
+//    public enum UserStatus
+//    {
+//        Inactive,
+//        Active,
+//        Draft
+//    }
+//    public enum Gender
+//    {
+//        M,
+//        F
+//    }
+//    public class User
+//    {
+//        public Gender Gender { get; set; }
+//        public string NCode { get; set; }
+//        public string? Name { get; set; }
+//        public string? LName { get; set; }
+//        public int Age { get; set; }
+//        //public bool Active { get; set; } 
+//        public UserStatus Status { get; set; } // 0 1 2
+//    }
+//}
