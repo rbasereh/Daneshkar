079bbd06a44071f435ebd4d84147ee07e8b88ede
Reza <rbasereh@gmail.com>
2023-08-19 19:57:40 +0330

    First Api in Web Api App

---
+++ Aspnetcore1/AspnetCoreWebApi/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AspnetCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            var listpersons = GetAllPersons();
+            return listpersons;
+        }
+
+        private List<Person> GetAllPersons()
+        {
+            List<Person> persons = new()
+            {
+                new Person() { Id = 1, Name = "Ali", LName = "Sadeghi" },
+                new Person() { Id = 2, Name = "Reza", LName = "Sadeghi" },
+                new Person() { Id = 3, Name = "Hasan", LName = "Alipour" }
+            };
+            return persons;
+        }
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LName { get; set; }
+    }
+}
