976331e29020c91c740936742fc0196ce77a007a
Reza <rbasereh@gmail.com>
2023-09-03 20:42:17 +0330

    order by ofType

--- aspnetcore3/LinqSample/Program.cs
+++ aspnetcore3/LinqSample/Program.cs
@@ -4,18 +4,12 @@
     {
         static void Main(string[] args)
         {
-
-
-
-            //
-            Person person1 = Search("akbar");
-            //Person person2 = Search("hasan");
-
-
+            SearchArrayList();
+            SearchPerson("");
 
             Console.WriteLine("Hello, World!");
         }
-        public static Person Search(string name)
+        public static Person SearchPerson(string name)
         {
             List<Person> personList = new()
             {
@@ -28,21 +22,27 @@
                 new Person(4, "reza", 50),
             };
 
-            //Person? result = personList.FirstOrDefault(e => e.Id == 6);
-            //Person? result2 = personList.LastOrDefault(e => e.Id == 6);
-            //Person? result = personList.Single(e => e.Id == 10);
-            Person? result = personList.SingleOrDefault(e => e.Id == 10);
-            /*
-                                               return 
-            First/Last                         1-n  => 1
-            FirstOrDefault/LastOrDefault       0-n  => 1
-            
-            Single                             1 
-            SingleOrDefault
+            //var result1 = personList.First(); //OrDefault
+            //var result2 = personList.Last(); //OrDefault
+            //var result3 = personList.Single(); // OrDefault
+            //var result4 = personList.OfType<Person>();
 
+            //OrderBy
+            //OrderByDescending
 
-            */
-            return result;
+            //ThenBy
+            //ThenByDescending
+
+            var newPersonList = personList.OrderBy(e => e.Age).ToList();
+
+            List<Person> Persons =
+            personList.OrderBy(e => e.Age).ThenByDescending(e => e.Name).ToList();
+
+            List<Person> Persons2 =
+                personList.OrderByDescending(e => e.Age).ToList();
+
+            var targetpersonList =
+                personList.Where(e => e.Name == "hasan").ToList();
 
 
             //for (int i = 0; i < personList.Count; i++)
@@ -62,6 +62,36 @@
             //     ).FirstOrDefault();
 
             return null;
+        }
+
+        public static void SearchArrayList()
+        {
+            List<object> list = new()
+            {
+                new Person(1, "ali", 30),
+                "test string",
+                10,
+                5,
+                new Person(2, "reza", 41),
+                new Person(3, "hasan", 45),
+            };
+            var AllPersonInlist2 =
+                list.Where(e => e.GetType() == typeof(Person)).ToList();
+
+            //int x = 1;
+            //Person x2 = new Person(3, "hasan", 45);
+
+            //Type result = x.GetType();
+            //Type result2 = x2.GetType(); 
+
+            List<Person> AllPersonInlist = list.OfType<Person>().ToList();
+
+
+            List<int> AllInetegerInlist = list.OfType<int>().ToList();
+
+            Person? person = list.OfType<Person>().FirstOrDefault(e => e.Name == "ali");
+
+
         }
 
         public static bool IsValid(Person person)
