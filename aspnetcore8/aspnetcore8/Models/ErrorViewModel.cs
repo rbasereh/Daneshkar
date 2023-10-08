b2c49bc94394c2b5eab2d76dd2d3c8268264bbc0
Reza <rbasereh@gmail.com>
2023-10-08 18:16:04 +0330

    init aspnetcore8

---
+++ aspnetcore8/aspnetcore8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnetcore8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
